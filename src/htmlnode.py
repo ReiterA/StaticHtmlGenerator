2daaf73f196e9bc7d09fe3d5fda4097ac1725e34
ReiterA <reiter.andreas24@gmail.com>
2026-03-06 23:25:10 +0100

    Implement LeafNode and ParentNode classes with to_html methods; add unit tests for HTMLNode functionality

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -15,3 +15,28 @@ class HTMLNode:
         if self.props is None or len(self.props) == 0:
             return ""
         return " " + ' '.join(f'{key}="{value}"' for key, value in self.props.items())
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str, value: str, props: dict = None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("All leaf nodes must have a value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag: str, children: list, props: dict = None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("All parent nodes must have a tag")
+
+        if self.children is None or len(self.children) == 0:
+            raise ValueError("All parent nodes must have children")
+
+        children_html = ''.join(child.to_html() for child in self.children)
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
