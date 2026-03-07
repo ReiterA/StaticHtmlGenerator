a8692cfd7eb69b610be04d1c00a45682d324a327
ReiterA <reiter.andreas24@gmail.com>
2026-03-07 22:41:49 +0100

    Add functions for extracting markdown images and links; implement split_nodes_image and split_nodes_link; enhance text_to_textnodes with new functionality

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,6 @@
+import re
 from textnode import TextNode, TextType
+
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type: TextType):
     new_nodes = []
@@ -18,3 +20,69 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type: TextType):
         else:
             new_nodes.append(node)
     return new_nodes
+
+def extract_markdown_images(text):
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+def extract_markdown_links(text):
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+def split_nodes_image(old_nodes):
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type == TextType.TEXT:
+            matches = extract_markdown_images(node.text)
+            if len(matches) > 0:
+                split_texts = re.split(pattern, node.text)
+                for i, split_text in enumerate(split_texts):
+                    if i % 3 == 1:
+                        alt_text = split_text
+                    if split_text == "":
+                        continue
+                    if i % 3 == 0:
+                        new_nodes.append(TextNode(split_text, TextType.TEXT))
+                    if i % 3 == 2:
+                        new_nodes.append(TextNode(alt_text, TextType.IMAGE, url=split_text))
+            else:
+                new_nodes.append(node)
+        else:
+            new_nodes.append(node)
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type == TextType.TEXT:
+            matches = extract_markdown_links(node.text)
+            if len(matches) > 0:
+                split_texts = re.split(pattern, node.text)
+                for i, split_text in enumerate(split_texts):
+                    if i % 3 == 1:
+                        alt_text = split_text
+                    if split_text == "":
+                        continue
+                    if i % 3 == 0:
+                        new_nodes.append(TextNode(split_text, TextType.TEXT))
+                    if i % 3 == 2:
+                        new_nodes.append(TextNode(alt_text, TextType.LINK, url=split_text))
+            else:
+                new_nodes.append(node)
+        else:
+            new_nodes.append(node)
+    return new_nodes
+
+def text_to_textnodes(text: str) -> list[TextNode]:
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+                
