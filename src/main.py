76a9136699d543f6a8176e1885d298957062eb91
ReiterA <reiter.andreas24@gmail.com>
2026-03-08 21:46:11 +0100

    Add blog posts on Glorfindel, The Lord of the Rings, and Tom Bombadil; update page generation logic and add contact information

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from inline_markdown import extract_title
 
 def main():
     copy_content("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
@@ -22,6 +22,15 @@ def generate_page(from_path, template_path, dest_path):
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as f:
         f.write(final_string)
-    
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        if filename.endswith(".md"):
+            from_path = os.path.join(dir_path_content, filename)
+            dest_path = os.path.join(dest_dir_path, filename.replace(".md", ".html"))
+            generate_page(from_path, template_path, dest_path)
+        if os.path.isdir(os.path.join(dir_path_content, filename)):
+            generate_pages_recursive(os.path.join(dir_path_content, filename), template_path, os.path.join(dest_dir_path, filename))
+
 if __name__ == "__main__":
     main()
